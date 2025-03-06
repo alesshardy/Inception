897ea70e4b8620bc37b1f21d3383d48594819b4f
apintus <apintus@apintus.apintus.42.fr>
2025-03-06 13:29:09 +0100

    push_from_vm_06031329

--- srcs/requirements/mariadb/conf/mdb-conf.sh
+++ srcs/requirements/mariadb/conf/mdb-conf.sh
@@ -10,11 +10,11 @@ sleep 5 # use to let mariadb time to start
 
 #-----------mariadb config-----------#
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \'${MYSQL_DB}\';"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DB\`;"
 
-mariadb -e "CREATE USER IF NOT EXISTS \'${MYSQL_USER}\'@'%' IDENTIFIED BY '${MYSQL_PASSWORD};"
+mariadb -e "CREATE USER IF NOT EXISTS \`$MYSQL_USER\`@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
-mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \'${MYSQL_USER}\'@'%';"
+mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO \`$MYSQL_USER\`@'%';"
 
 mariadb -e "FLUSH PRIVILEGES;"
 
@@ -22,4 +22,4 @@ mariadb -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
-mysql_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql/'
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql/'
--- srcs/requirements/wordpress/conf/wp-conf.sh
+++ srcs/requirements/wordpress/conf/wp-conf.sh
@@ -15,15 +15,34 @@ chmod -R 755 /var/www/wordpress
 
 chown -R www-data:www-data /var/www/wordpress
 
-# wp core installation
-wp core download --allow-root
+# Wait for MariaDB to be ready
+until mariadb -h mariadb -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;" > /dev/null 2>&1; do
+    echo "Waiting for MariaDB to be ready..."
+    sleep 5
+done
 
-wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser=="$MYSQL_USER" \
-                --dbpass="$MYSQL_PASSWORD" --allow-root
+# Remove existing WordPress files
+rm -rf /var/www/wordpress/*
 
-wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_NAME" \
-                --admin_passeword="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+# Check if wp-config.php exists
+if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
+    # wp core installation
+    wp core download --allow-root
+
+    wp config create --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" \
+                     --dbpass="$MYSQL_PASSWORD" --dbhost="mariadb:3306" --allow-root
+
+    wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_NAME" \
+                    --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+else
+    echo "WordPress is already installed."
+fi
 
 #-----------php config-----------#
 
-php-fpm7.4 -F
+# change listen port from unix socket to 9000
+sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
+# create a directory for php-fpm
+mkdir -p /run/php
+# start php-fpm service in the foreground to keep the container running
+/usr/sbin/php-fpm7.4 -F
